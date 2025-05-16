f20f12b1570ad04aecb04a39c4d06b0bbc56c029
hasnahatti <127605934+hasnahatti70@users.noreply.github.com>
2025-05-16 12:35:36 +0200

    Create GreetingServiceTest.java

---
+++ formulaire/src/test/java/com/example/formulaire/GreetingServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.formulaire;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GreetingServiceTest {
+
+    @Test
+    public void testGreeting() {
+        String expected = "Bonjour Hasna";
+        String result = "Bonjour Hasna"; // remplace ici par une vraie méthode plus tard
+        assertEquals(expected, result);
+    }
+}
