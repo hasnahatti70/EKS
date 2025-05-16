83b0cec5990811daaae9fdf765bae6923403fa64
hasnahatti <127605934+hasnahatti70@users.noreply.github.com>
2025-05-16 11:47:45 +0200

    Update ExampleTest.java

--- src/test/java/com/example/formulaire/ExampleTest.java
+++ src/test/java/com/example/formulaire/ExampleTest.java
@@ -1,15 +1,12 @@
 package com.example.formulaire;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ExampleTest {
+class ExampleTest {
 
     @Test
-    public void testAddition() {
-        int a = 2;
-        int b = 3;
-        int result = a + b;
-        assertEquals(5, result);
+    void simpleTest() {
+        assertTrue(true); // Ce test passe toujours
     }
 }
