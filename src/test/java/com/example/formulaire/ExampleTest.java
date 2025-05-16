435308f00e34568348b658b407159d3362c5ca2c
hasnahatti <127605934+hasnahatti70@users.noreply.github.com>
2025-05-16 11:05:54 +0200

    Create ExampleTest.java
    add: simple unit test ExampleTest.java

---
+++ src/test/java/com/example/formulaire/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.example.formulaire;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        int a = 2;
+        int b = 3;
+        int result = a + b;
+        assertEquals(5, result);
+    }
+}
